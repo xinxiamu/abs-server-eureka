5854feabccc07375880287c3398dea5679437494
xinxiamu <932852117@qq.com>
2018-01-24 14:16:36 +0800

    基础配置调整

---
+++ src/test/java/com/ymu/abs/absservereureka/AbsServerEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ymu.abs.absservereureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbsServerEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
