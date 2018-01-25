40e8c41a2a19bfe5a2d048438a97321a527645d1
xinxiamu <932852117@qq.com>
2018-01-25 22:40:34 +0800

    基础配置调整

---
+++ src/main/java/com/ymu/abs/servereureka/AbsServerEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.ymu.abs.servereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AbsServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsServerEurekaApplication.class, args);
+	}
+}
