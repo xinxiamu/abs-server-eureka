674ced44a9069f14292bb5caa2dc8d97e9c8a2fd
xinxiamu <932852117@qq.com>
2018-01-26 11:48:29 +0800

    基础配置调整

--- src/main/java/com/abs/servereureka/AbsServerEurekaApplication.java
+++ src/main/java/com/abs/servereureka/AbsServerEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.ymu.abs.servereureka;
+package com.abs.servereureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/abs/servereureka/AbsServerEurekaApplicationTests.java
+++ src/test/java/com/abs/servereureka/AbsServerEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ymu.abs.absservereureka;
+package com.abs.servereureka;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
